6b9d6930ac3e6a25e06b9c78747cdbe4be2fbb7b
acesulfamK <sweetc4h4kno4s@gmail.com>
2024-09-05 15:45:15 +0900

    fix setup3.sh

--- setup/setup3.sh
+++ setup/setup3.sh
@@ -1,5 +1,7 @@
 # I have not tested this script yet.
 export PATH=$PATH:/opt/conda/bin
-mv ./setup/requirement.txt /opt/pytorch
+mkdir /opt/pytorch/
+cp ./setup/requirements.txt /opt/pytorch/
 cd /opt/pytorch
 pip install -U pip && pip install -r requirements.txt
+cd ~
