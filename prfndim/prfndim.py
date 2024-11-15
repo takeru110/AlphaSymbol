93f3a6cb9184a84ebdcb224c64863138f1325491
acesulfamK <sweetc4h4kno4s@gmail.com>
2024-11-15 15:28:15 +0900

    add Z

---
+++ prfndim/prfndim.py
@@ -0,0 +1,28 @@
+class Expr:
+    _instances: dict[int, int] = {}
+
+    def __new__(cls, *args):
+        key = hash((cls, *args))
+        if key in cls._instances:
+            return cls._instances[key]
+        instance = super().__new__(cls)
+        cls._instances[key] = instance
+        return instance
+
+    def eval(self, *args: int):
+        raise NotImplementedError()
+
+
+class Z(Expr):
+    def __init__(self):
+        self._arity = None
+
+    def eval(self, *args: int) -> int:
+        return 0
+
+    def __str__(self) -> str:
+        return "Z()"
+
+    @property
+    def arity(self):
+        return self._arity
