d4f244719b1f758ecd53757519846466ad3f119e
acesulfamK <sweetc4h4kno4s@gmail.com>
2024-11-15 21:43:22 +0900

    add arity and modify R.__Init__()

--- prfndim/prfndim.py
+++ prfndim/prfndim.py
@@ -27,6 +27,9 @@ class Expr:
     def __str__(self) -> str:
         raise NotImplementedError()
 
+    def arity(self):
+        raise NotImplementedError()
+
 
 class Z(Expr):
     def eval(self, *args: int) -> int:
@@ -59,6 +62,10 @@ class P(Expr):
     def __str__(self) -> str:
         return f"P({self._n}, {self._i})"
 
+    @property
+    def arity(self):
+        return self._n
+
 
 class C(Expr):
     def __init__(self, *args: Expr):
@@ -72,6 +79,11 @@ class C(Expr):
         args_str = ", ".join(str(arg) for arg in self._args)
         return f"C({str(self._base)}, {args_str})"
 
+    @property
+    def arity(self):
+        """This function needs that "self" is valid semantically."""
+        return self._args[0].arity
+
 
 class R(Expr):
     def __init__(self, *args: Expr):
@@ -81,10 +93,28 @@ class R(Expr):
         if len(args) % 2 == 0:
             raise PrfSyntaxError("the number of sub Exprs of R() must be odd")
 
-        self._dim: int = (len(args) - 1) // 2
-        self._base: Expr = args[0]
-        self._args: tuple[Expr, ...] = args[1:]
+        self._dim: int = (len(args) - 1) // 2  # the num of step functions
+        self._term = args[0]
+        self._steps: tuple[Expr, ...] = args[1 : self._dim + 1]
+        self._bases: tuple[Expr, ...] = args[self._dim + 1 :]
+        self._arity = self._calc_arity()
 
     def __str__(self) -> str:
-        args_str = ", ".join(str(arg) for arg in self._args)
-        return f"R({str(self._base)}, {args_str})"
+        steps_str = ", ".join(str(step) for step in self._steps)
+        bases_str = ", ".join(str(base) for base in self._bases)
+        return f"R({str(self._term)}, {steps_str}, {bases_str})"
+
+    def _calc_arity(self):
+        """This function is called when initializing this object."""
+        for base in self._bases:
+            if base.arity is not None:
+                return base.arity + 1
+        for step in self._steps:
+            if step.arity is not None:
+                return step.arity - self._dim
+        return None
+
+    @property
+    def arity(self):
+        """This function needs that "self" is valid semantically."""
+        return self._arity
