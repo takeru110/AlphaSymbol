fc1f1a0be09c7eb4898145718fa23e61a2dd192a
acesulfamK <sweetc4h4kno4s@gmail.com>
2024-12-09 16:52:46 +0900

    fixup

--- src/models/lightning_modules.py
+++ src/models/lightning_modules.py
@@ -1,7 +1,9 @@
 import lightning as L
+import pandas as pd
 from torch import Tensor, nn, optim, utils
+from train import TransformerDataset, TransformerModel
 
-from models import PositionalEncoding, TransformerModel
+from models import PositionalEncoding
 
 
 class LitTransformer(L.LightningModule):
@@ -29,14 +31,31 @@ class LitTransformer(L.LightningModule):
         output = self.model(src_batch, tgt_input)  # (T, N, C)
         output = output.permute(1, 2, 0)  # (N, C, T)
         loss = self.loss_fn(output, tgt_output)
-        loss.backward()
         return loss
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.learning_rate)
 
+
 data_path = "/home/takeru/AlphaSymbol/data/prfndim/d3-a2-c3-r3-status.csv"
 models_output_dir = "./temp/"
 config_dir = "./temp/"
-model = TransformerModel()
-lightning_module = LitTransformer(
+df = pd.read_csv(data_path)
+dataset = TransformerDataset(df)
+dataloadr = utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
+
+model = TransformerModel(
+    src_vocab_size=len(dataset.src_vocab),
+    tgt_vocab_size=len(dataset.tgt_vocab),
+    src_max_len=dataset.src_max_len,
+    tgt_max_len=dataset.tgt_max_len,
+    d_model=64,
+    nhead=4,
+    num_encoder_layers=6,
+    num_decoder_layers=6,
+    dim_feedforward=512,
+    dropout=0.1,
+)
+lightning_module = LitTransformer(model, learning_rate=0.0001)
+trainer = L.Trainer(max_epochs=100, accelerator="gpu", devices=1)
+trainer.fit(lightning_module, dataloadr)
